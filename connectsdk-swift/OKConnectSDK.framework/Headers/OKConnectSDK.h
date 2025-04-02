44ff258e13e70a56f8c5055479d5bec3aac9f0c2
fanghao.shu <fanghao.shu@okg.com>
2025-04-02 15:39:20 +0800

    init upload

---
+++ connectsdk-swift/OKConnectSDK.framework/Headers/OKConnectSDK.h
@@ -0,0 +1,18 @@
+//
+//  OKConnectSDK.h
+//  OKConnectSDK
+//
+//  Created by oker on 2025/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OKConnectSDK.
+FOUNDATION_EXPORT double OKConnectSDKVersionNumber;
+
+//! Project version string for OKConnectSDK.
+FOUNDATION_EXPORT const unsigned char OKConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OKConnectSDK/PublicHeader.h>
+
+
